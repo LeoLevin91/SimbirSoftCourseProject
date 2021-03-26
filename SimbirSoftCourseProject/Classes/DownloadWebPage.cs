4f1f1f4292fd1a3c2121a1894698e5bff854e4a5
Leonard <RengersPetrov18>
2021-03-26 19:16:34 +0400

    Progress: -> UML class diagram -> Three class templates

---
+++ SimbirSoftCourseProject/Classes/DownloadWebPage.cs
@@ -0,0 +1,12 @@
+﻿namespace SimbirSoftCourseProject.Classes
+{
+    /*
+     * Данный класс производит загрузку WEB-страницы по указанному URL адресу
+     * с помощью HTTP запроса.
+     * Так же данный класс сохраняет скаченную страницу в текстовый документ
+     */
+    public class DownloadWebPage
+    {
+        
+    }
+}
---
+++ SimbirSoftCourseProject/Classes/MainApplication.cs
@@ -0,0 +1,12 @@
+﻿namespace SimbirSoftCourseProject.Classes
+{
+    /*
+     * Данный класс агрегирует (композиция) в себе два других класса (<DownloadWebPage>, <CounterWord>)
+     * А также производит логгирование ошибок в отдельный файл
+     */
+    public class MainApplication
+    {
+        public DownloadWebPage downloadWebPage { get; set; } = new DownloadWebPage();
+        public CounterWord counterWord { get; set; } = new CounterWord();
+    }
+}
